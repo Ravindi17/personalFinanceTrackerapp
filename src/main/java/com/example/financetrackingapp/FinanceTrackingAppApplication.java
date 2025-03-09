90384f859dd2c33ee25aa61ed14abff58703dbc7
Ravindi Welagedara <ravindiwelagedara@gmail.com>
2025-03-09 10:50:52 +0530

    added the aaplication.java file

---
+++ src/main/java/com/example/financetrackingapp/FinanceTrackingAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.financetrackingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackingAppApplication.class, args);
+	}
+}
